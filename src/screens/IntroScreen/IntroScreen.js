f2a60a3e7c889df48d0577cdd94bac1b18ea7472
Ahmet Güneş <ahmetgunes.ceng@gmail.com>
2024-10-01 19:23:30 +0300

    backup

--- src/screens/IntroScreen/IntroScreen.js
+++ src/screens/IntroScreen/IntroScreen.js
@@ -1,10 +1,12 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, ImageBackground } from "react-native";
 import React from "react";
 
 export default function IntroScreen() {
   return (
-    <View>
-      <Text>IntroScreen</Text>
-    </View>
+    <TouchableOpacity>
+      <ImageBackground>
+        <Text>adasd</Text>
+      </ImageBackground>
+    </TouchableOpacity>
   );
 }
